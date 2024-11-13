7d861c616df3cd50383d1c2d457e726082132d79
Dudziecki <mkdudik15@gmail.com>
2024-11-13 20:30:00 +0300

    Start Testimony

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,12 +3,13 @@ import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 import styled from "styled-components";
 
+const Items = ["Home", "Skills", "Works", "Testimony", "Contacts"]
 export const Header = () => {
 
     return (
         <StyledHeader>
             <Logo/>
-            <Menu/>
+            <Menu menuItems={Items}/>
         </StyledHeader>
     );
 
