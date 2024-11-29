460a264a60253a75b30368b8d0bbd169928a1482
Dudziecki <mkdudik15@gmail.com>
2024-11-29 23:09:11 +0300

    Email sending is done

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {ElementRef, useRef} from 'react';
 import {S} from "../contact/Contacts_Styles"
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../styles/Container";
+import emailjs from '@emailjs/browser';
 
 
 export const Contact:React.FC = () => {
-    return (
-        <S.Contact id={'contacts'}>
-            <Container>
-                <SectionTitle>Contact</SectionTitle>
-                <S.Form>
-                    <S.Field placeholder={"Name"}/>
-                    <S.Field placeholder={"Subject"}/>
-                    <S.Field placeholder={"Message"} as={"textarea"}/>
-                    <Button type={"submit"}>Send Message</Button>
-                </S.Form>
-            </Container>
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        if (!form.current) return
+        emailjs
+            .sendForm('service_0jqzslj', 'template_uf2dn3y', form.current, {
+                publicKey: 'ZjRr3DV0f42Fk6yH-',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+
+            );
+        e.target.reset()
+    };
+        return (
+            <S.Contact id={'contacts'}>
+                <Container>
+                    <SectionTitle>Contact</SectionTitle>
+                    <S.Form ref={form} onSubmit={sendEmail}>
+                        <S.Field required={true} placeholder={"Name"} name={"user_name"}/>
+                        <S.Field required={true} placeholder={"Email"} name={"user_email"}/>
+                        <S.Field required={true} placeholder={"Subject"} name={"subject"} />
+                            <S.Field required={true} placeholder={"Message"} as={"textarea"} name={"message"} />
+                            <Button type={"submit"}>Send Message</Button>
+                    </S.Form>
+                </Container>
 
 
-        </S.Contact>
+            </S.Contact>
     );
-};
+
+    }
 
